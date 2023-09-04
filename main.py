102394a2111eff1c6056f9990f8f2e84d4cc22dc
Mason Bassett <Bassettmason@gmail.com>
2023-09-04 20:30:38 +0000

    ci cd and docker file added

--- main.py
+++ main.py
@@ -12,4 +12,5 @@ def page():
     return render_template("page.html")  # This renders the page.html template
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=80)
+
